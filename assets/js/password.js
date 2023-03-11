abdeb23c8e7424fa73119f384b8b4a2a935a5318
JDdevelopp <jagratadeb2007@gmail.com>
2023-03-11 23:39:09 +0530

    Update password.js

--- assets/js/password.js
+++ assets/js/password.js
@@ -1,28 +1,40 @@
 function passWord() {
     var testV = 1;
-    var pass1 = prompt('🌹Please Enter The Password:','');
+    var pass1 = prompt('🌹Please Enter Your First Name Only (Case Sensitive):','');
     while (testV < 3) {
         if (!pass1) history.go(-1);
-        if (pass1.toLowerCase() == "iamuseless") {
+        if (pass1.toLowerCase() == "Jagrata") {
             alert('✅ Password Matched!');
             window.close("https://jddevelopp.github.io/photogallery/")
             window.open('https://jddevelopp.github.io/photogallery/home.html');
             break;
         }
-        if (pass1.toLowerCase() == "ok") {
+        if (pass1.toLowerCase() == "Siddharth") {
             alert('✅ Password Matched!');
             window.close("https://jddevelopp.github.io/photogallery/")
             window.open('https://jddevelopp.github.io/photogallery/home.html');
             break;
         }
-        if (pass1.toLowerCase() == "i") {
+        if (pass1.toLowerCase() == "Radhika") {
+            alert('✅ Password Matched!');
+            window.close("https://jddevelopp.github.io/photogallery/")
+            window.open('https://jddevelopp.github.io/photogallery/home.html');
+            break;
+        } 
+        if (pass1.toLowerCase() == "Shreya") {
+            alert('✅ Password Matched!');
+            window.close("https://jddevelopp.github.io/photogallery/")
+            window.open('https://jddevelopp.github.io/photogallery/home.html');
+            break;
+        } 
+        if (pass1.toLowerCase() == "Yashoda") {
             alert('✅ Password Matched!');
             window.close("https://jddevelopp.github.io/photogallery/")
             window.open('https://jddevelopp.github.io/photogallery/home.html');
             break;
         } 
         testV+=1;
-        var pass1 = prompt('❌ Access Denied - Incorrect Password, Please Try Again.','Password');
+        var pass1 = prompt('❌ Access Denied - This name is not registered for access. Contact Jagrata for getting your name registered.','Password');
     }
     if (pass1.toLowerCase()!="password" & testV ==3) history.go(-1);
     return "";
